9c9fc5696eb745171b0a03c3e9b7ead8f944fe00
DayanaNavarro <navarrodayana6@gmail.com>
2022-09-01 16:16:44 -0500

    Agregue los elementos

---
+++ functions.py
@@ -0,0 +1,14 @@
+Python 3.7.2 (v3.7.2:9a3ffc0492, Dec 24 2018, 02:59:38) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> """
+# -- --------------------------------------------------------------------------------------------------- -- #
+# -- project: A SHORT DESCRIPTION OF THE PROJECT                                                         -- #
+# -- script: functions.py : python script with general functions                                         -- #
+# -- author: YOUR GITHUB USER NAME                                                                       -- #
+# -- license: GPL-3.0 License                                                                            -- #
+# -- repository: YOUR REPOSITORY URL                                                                     -- #
+# -- --------------------------------------------------------------------------------------------------- -- #
+"""
+
